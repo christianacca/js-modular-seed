d0c9c928c7ff3ea44ae407dc8591df3ae3b137bf
Christian Crowhurst <ccrowhurst@ramplc.com>
2016-07-17 08:23:33 +0100

    workaround for incorrect node_module resolves
    Without this workaround, webpack will resolve lib1 import for lodash using
    the lodash package in the project root. Whereas it should be using the
    lodash package from app-webpack/node_modules.
    
    This solution still has problems:
    
    EG1: lib2 could have a dependency on core-js@1.0.0 whereas app-webpack
    could have a dependency on core-js@1.0.1. The resolve will use core-js1.0.1
    for imports that it sees for lib2
    
    EG2: lib2 could have a dependency on core-js@1.0.0 whereas lib1
    could have a dependency on core-js@1.0.1. The resolve will use core-js1.0.1
    for imports that it sees for lib2

--- webpack-common/appParts.js
+++ webpack-common/appParts.js
@@ -1,10 +1,19 @@
 const merge = require('webpack-merge');
 const path = require('path');
+const webpack = require('webpack');
 const commonParts = require('./parts');
 
 module.exports = createAppParts;
 
 function createAppParts(sourceDir, isProd) {
+    const PATHS = {
+        build: path.join(sourceDir, 'build'),
+        project: path.resolve(sourceDir, '../')
+    };
+
+    const rootPkg = require(path.resolve(PATHS.project, 'package'));
+    const pkg = require(path.join(sourceDir, 'package'));
+
 
     return Object.assign({}, commonParts, {
         asAppBundle,
@@ -13,6 +22,23 @@ function createAppParts(sourceDir, isProd) {
     });
 
     /////
+
+    function getLibraryNames() {
+        const projectScopeName = `@${rootPkg.name}`;
+        return Object.keys(pkg.dependencies)
+            .filter(name => name.startsWith(projectScopeName))
+            .map(name => name.split('/')[1]);
+    }
+
+    function moduleResolvePaths() {
+        let libraryNodeModulesPath = getLibraryNames()
+            .map(name => path.join(PATHS.project, 'lib', name, 'node_modules'));
+        return {
+            resolve: {
+                modules: [sourceDir, path.join(sourceDir, 'node_modules'), ...libraryNodeModulesPath]
+            }
+        }
+    }
 
     function useHtmlPlugin() {
         var HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -24,13 +50,31 @@ function createAppParts(sourceDir, isProd) {
     }
 
     function asAppBundle() {
-        const filename = isProd ? 'bundle.min.js' : 'bundle.js';
-        return {
-            entry: path.join(sourceDir, 'index.js'),
-            output: {
-                path: path.join(sourceDir, 'dist'),
-                filename: filename
-            }
-        };
+        const isNodeModule = new RegExp('node_modules');
+        return merge(
+            {
+                entry: {
+                    app: path.join(sourceDir, 'index.js')
+                },
+                output: {
+                    path: PATHS.build,
+                    filename: '[name].[chunkhash].js',
+                    // This is used for require.ensure if/when we want to use it
+                    chunkFilename: '[chunkhash].js'
+                },
+                plugins: [
+                    new webpack.optimize.CommonsChunkPlugin({
+                        name: 'vendor',
+                        minChunks: module => isNodeModule.test(module.resource)
+                    }),
+                    // ensure vendor hash does not change (as per solution discuess here https://github.com/webpack/webpack/issues/1315)
+                    new webpack.optimize.CommonsChunkPlugin({
+                        name: 'manifest',
+                        minChunks: Infinity
+                    })
+                ]
+            },
+            moduleResolvePaths()
+        );
     }
 }
